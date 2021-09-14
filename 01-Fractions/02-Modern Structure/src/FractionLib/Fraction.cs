3c405f288249d20c8853cb288a45df54ec9015db
Dmitry Barabash <dmitry@barabash.com>
2021-09-14 22:04:28 +0300

    Add modern project structure for Fraction sample

---
+++ 01-Fractions/02-Modern Structure/src/FractionLib/Fraction.cs
@@ -0,0 +1,239 @@
+﻿using System;
+
+namespace FractionLib
+{
+    public class Fraction
+    {
+        public int Numerator { get; private set; }
+        public int Denumerator { get; private set; }
+
+        public Fraction(int numerator = 0, int denumerator = 1)
+        {
+            Set(numerator, denumerator);
+        }
+
+        public void Set(int numerator = 0, int denumerator = 1)
+        {
+            Numerator = denumerator != 0 ? numerator : 0;
+            Denumerator = denumerator != 0 ? denumerator : 1;
+        }
+
+        public override string ToString()
+        {
+            return $"{Numerator}/{Denumerator}";
+        }
+
+
+        //
+        // Перегрузка унарных операций
+        //
+
+        public static Fraction operator -(Fraction fr)
+        {
+            return new Fraction(-fr.Numerator, fr.Denumerator);
+        }
+
+        public static Fraction operator ++(Fraction fr)
+        {
+            return fr + 1;
+        }
+
+        public static Fraction operator --(Fraction fr)
+        {
+            return fr - 1;
+        }
+
+
+
+        //
+        // Перегрузка бинарных операций
+        //
+
+        public static Fraction operator +(Fraction fr1, Fraction fr2)
+        {
+            return new Fraction(
+                fr1.Numerator * fr2.Denumerator + fr1.Denumerator * fr2.Numerator,
+                fr1.Denumerator * fr2.Denumerator);
+        }
+
+        public static Fraction operator +(Fraction fr, int n)
+        {
+            return new Fraction(
+                fr.Numerator + fr.Denumerator * n,
+                fr.Denumerator);
+        }
+
+        public static Fraction operator +(int n, Fraction fr)
+        {
+            return fr + n;
+        }
+
+        public static Fraction operator -(Fraction fr1, Fraction fr2)
+        {
+            return new Fraction(
+                fr1.Numerator * fr2.Denumerator - fr1.Denumerator * fr2.Numerator,
+                fr1.Denumerator * fr2.Denumerator);
+        }
+
+        public static Fraction operator -(Fraction fr, int n)
+        {
+            return new Fraction(
+                fr.Numerator - fr.Denumerator * n,
+                fr.Denumerator);
+        }
+
+        public static Fraction operator -(int n, Fraction fr)
+        {
+            return fr - n;
+        }
+
+        public static Fraction operator *(Fraction fr1, Fraction fr2)
+        {
+            return new Fraction(
+                fr1.Numerator * fr2.Numerator,
+                fr1.Denumerator * fr2.Denumerator);
+        }
+
+        public static Fraction operator *(Fraction fr, int n)
+        {
+            return new Fraction(fr.Numerator + n, fr.Denumerator);
+        }
+
+        public static Fraction operator *(int n, Fraction fr)
+        {
+            return fr * n;
+        }
+
+        public static Fraction operator /(Fraction fr1, Fraction fr2)
+        {
+            return new Fraction(
+                fr1.Numerator * fr2.Denumerator,
+                fr1.Denumerator * fr2.Numerator);
+        }
+
+        public static Fraction operator /(Fraction fr, int n)
+        {
+            return new Fraction(fr.Numerator, fr.Denumerator * n);
+        }
+
+        public static Fraction operator /(int n, Fraction fr)
+        {
+            return new Fraction(n * fr.Denumerator, fr.Numerator);
+        }
+
+
+        //
+        // Перегрузка операций сравнения >, <, >= и <=
+        //
+
+        public static bool operator >(Fraction fr1, Fraction fr2)
+        {
+            return (double)fr1.Numerator / fr1.Denumerator > (double)fr2.Numerator / fr2.Denumerator;
+        }
+
+        public static bool operator <(Fraction fr1, Fraction fr2)
+        {
+            return (double)fr1.Numerator / fr1.Denumerator < (double)fr2.Numerator / fr2.Denumerator;
+        }
+
+        public static bool operator >=(Fraction fr1, Fraction fr2)
+        {
+            return (double)fr1.Numerator / fr1.Denumerator >= (double)fr2.Numerator / fr2.Denumerator;
+        }
+
+        public static bool operator <=(Fraction fr1, Fraction fr2)
+        {
+            return (double)fr1.Numerator / fr1.Denumerator <= (double)fr2.Numerator / fr2.Denumerator;
+        }
+
+
+        //
+        // Перегрузка операций сравнения == и !=
+        //
+        // При перегрузке любой из этих операций рекомендуется переопределять так же
+        // унаследованные от класса Object методы GetHashCode() и Equals(object)
+        //
+
+        public static bool operator ==(Fraction fr1, Fraction fr2)
+        {
+            return (double)fr1.Numerator / fr1.Denumerator == (double)fr2.Numerator / fr2.Denumerator;
+        }
+
+        public static bool operator !=(Fraction fr1, Fraction fr2)
+        {
+            return (double)fr1.Numerator / fr1.Denumerator != (double)fr2.Numerator / fr2.Denumerator;
+        }
+
+        public override int GetHashCode()
+        {
+            //
+            // Классический вариант из Рихтера
+            //
+            //unchecked
+            //{
+            //    return (Numerator * 397) ^ Denumerator;
+            //}
+
+            //
+            // Используем класс HashCode, добавленный в .NET Core 2.1
+            //
+            return HashCode.Combine(Numerator, Denumerator);
+        }
+
+        public override bool Equals(object other)
+        {
+            if (other is null) return false;
+            if (object.ReferenceEquals(this, other)) return true;
+            if (GetType() != other.GetType()) return false;
+            return Equals(other as Fraction);
+        }
+
+        protected bool Equals(Fraction other)
+        {
+            return ((double)Numerator / Denumerator).CompareTo((double)other.Numerator / other.Denumerator) == 0;
+        }
+
+
+        //
+        // Перегрузка логических операций
+        //
+        // Явно перегрузить &&, || и ?: нельзя, но можно их перегрузить неявно,
+        // перегрузив операции &, |, true, и false
+        //
+
+        public static bool operator !(Fraction fr)
+        {
+            return fr.Numerator == 0;
+        }
+
+        public static bool operator &(Fraction fr1, Fraction fr2)
+        {
+            return fr1.Numerator != 0 && fr2.Numerator != 0;
+        }
+
+        public static bool operator |(Fraction fr1, Fraction fr2)
+        {
+            return fr1.Numerator != 0 || fr2.Numerator != 0;
+        }
+
+        public static bool operator true(Fraction fr)
+        {
+            return fr.Numerator != 0;
+        }
+
+        public static bool operator false(Fraction fr)
+        {
+            return fr.Numerator == 0;
+        }
+
+
+        //
+        // Перегрузка операций преобразования
+        //
+
+        public static explicit operator double(Fraction fr)
+        {
+            return (double)fr.Numerator / fr.Denumerator;
+        }
+    }
+}
---
+++ 01-Fractions/02-Modern Structure/tests/FractionLib.Tests.xUnit/FractionTests.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Xunit;
+
+namespace FractionLib.Tests.xUnit
+{
+    public class FractionTests
+    {
+        [Fact]
+        public void OperatorEquals_1_2And1_2_ReturnsTrue()
+        {
+            // Arrange
+            var a = new Fraction(1, 2);
+            var b = new Fraction(1, 2);
+
+            // Act
+            var actual = a == b;
+
+            // Assert
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void OperatorEquals_1_2And2_2_ReturnsFalse()
+        {
+            // Arrange
+            var a = new Fraction(1, 2);
+            var b = new Fraction(2, 2);
+
+            // Act
+            var actual = a == b;
+
+            // Assert
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void OperatorPlus_1_2And1_2_Returns4_4()
+        {
+            // Arrange
+            var a = new Fraction(1, 2);
+            var b = new Fraction(1, 2);
+            var expected = new Fraction(4, 4);
+
+            // Act
+            var actual = a + b;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(1, 2, 1, 2, 4, 4)]
+        [InlineData(1, 2, 1, 3, 5, 6)]
+        [InlineData(1, 3, 1, 2, 5, 6)]
+        [InlineData(3, 2, 1, 2, 8, 4)]
+        [InlineData(1, 2, 3, 2, 8, 4)]
+        public void OperatorPlus_AddTwoFractions_ReturnsResult(
+            int f1num, int f1denum,
+            int f2num, int f2denum,
+            int expectedNum, int expectedDenum)
+        {
+            // Arrange
+            var a = new Fraction(f1num, f1denum);
+            var b = new Fraction(f2num, f2denum);
+            var expected = new Fraction(expectedNum, expectedDenum);
+
+            // Act
+            var actual = a + b;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory, ClassData(typeof(FractionDataClass))]   // Arrange
+        public void OperatorPlus_AddTwoFractions(Fraction a, Fraction b, Fraction expected)
+        {
+            // Act
+            var actual = a + b;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+
+    public class FractionDataClass : IEnumerable<object[]>
+    {
+        private readonly List<object[]> _data = new()
+        {
+            new object[] { new Fraction(1, 2), new Fraction(1, 2), new Fraction(4, 4) },
+            new object[] { new Fraction(1, 2), new Fraction(1, 3), new Fraction(5, 6) },
+            new object[] { new Fraction(1, 3), new Fraction(1, 2), new Fraction(5, 6) },
+            new object[] { new Fraction(3, 2), new Fraction(1, 2), new Fraction(8, 4) },
+            new object[] { new Fraction(1, 2), new Fraction(3, 2), new Fraction(8, 4) }
+        };
+
+        public IEnumerator<object[]> GetEnumerator() => _data.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
